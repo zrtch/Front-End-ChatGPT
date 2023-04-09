d889816425973c159d36df792c3d65922cf94450
zrtch <1160639597@qq.com>
2023-04-09 23:08:08 +0800

    updater

--- demo.js
+++ demo.js
@@ -212,3 +212,46 @@ function outerFunction() {
 
 const closure = outerFunction() // 通过外部函数调用获得内部函数
 closure()  // 输出 "I am from outer function"
+
+// 模拟实现一个 Promise 类
+class MyPromise {
+  constructor(executor) {
+    this.state = 'pending'
+    this.value = undefined
+    this.reason = undefined
+
+    const resolve = (value) => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled'
+        this.value = value
+      }
+    }
+
+    const reject = (reason) => {
+      if (this.state === 'pending') {
+        this.state = 'rejected'
+        this.reason = reason
+      }
+    }
+
+    try {
+      executor(resolve, reject)
+    } catch (err) {
+      reject(err)
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    if (this.state === 'fulfilled') {
+      onFulfilled(this.value)
+    } else if (this.state === 'rejected') {
+      onRejected(this.reason)
+    }
+  }
+
+  catch(onRejected) {
+    if (this.state === 'rejected') {
+      onRejected(this.reason)
+    }
+  }
+}
